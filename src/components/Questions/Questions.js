4aefb47f5f62ef9ef78f3be326695317cdfbb75e
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-03-26 02:04:18 +0600

    quistions answered

--- src/components/Questions/Questions.js
+++ src/components/Questions/Questions.js
@@ -7,37 +7,21 @@ const Questions = () => {
                 <Accordion.Item eventKey="0">
                     <Accordion.Header>How React Works?</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.
+                        React makes a clone of the browser DOM to display changes quickly. When any state changes on browser React quickly understand and identify the changes using diffing algorithm. Then React very quickly re-render the Virtual DOM to display the changes.
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="1">
                     <Accordion.Header>Difference between Props vs State? </Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.
+                        1. Props are immutable while State is mutable.
+                        <br />
+                        2. Props used to pass data to child components while State is not accessible from child components.
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="2">
                     <Accordion.Header>How React Use States Works? </Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.
+                        In React all components has a state object where current state is stored. When any changes occurs on any component values, then the previously stored state value changes. Then React re-render the components to display the changes.
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
