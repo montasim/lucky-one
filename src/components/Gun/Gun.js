bbf3b100f8e83caf82fd2fdc58cd234bec36ea83
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-03-25 23:52:20 +0600

    single gun & sidebar

--- src/components/Gun/Gun.js
+++ src/components/Gun/Gun.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 const Gun = ({ data }) => {
     const { action, bullet, capacity, category, id, img, name, price } = data;
 
     return (
-        <div>
-            <h1>{name}</h1>
+        <div className='col-11 col-md-5 col-lg-4 p-2'>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img className='p-5' variant="top" src={img} />
+                <Card.Body className='mt-2'>
+                    <Card.Title>{name}</Card.Title>
+                    <div className='mt-4'>
+                        <p>Action: {action}</p>
+                        <p>Bullet: {bullet}</p>
+                        <p>Capacity: {capacity}</p>
+                        <p>Category: {category}</p>
+                    </div>
+                    <div className='d-flex align-items-center justify-content-between'>
+                        <Button variant="primary">Add to Cart</Button>
+                        <h3 className='me-1'>${price}</h3>
+                    </div>
+                </Card.Body>
+            </Card>
         </div>
     );
 };
--- src/components/Guns/Guns.js
+++ src/components/Guns/Guns.js
@@ -11,10 +11,25 @@ const Guns = () => {
     }, [])
 
     return (
-        <div>
-            {
-                guns.map(gun => <Gun key={gun.id} data={gun}></Gun>)
-            }
+        <div className='my-5 row mx-auto px-5'>
+            <div className='text-center mb-5'>
+                <h1 className='text-info'>Gun of Your Dreams </h1>
+                <h4 className='mt-3s'>Choose Your 4 Favourite Gun From Our Large Collection</h4>
+            </div>
+            <div className='container col-lg-9 col-11 gap-3 mx-auto'>
+                <div className='row'>
+                    {
+                        guns.map(gun => <Gun key={gun.id} data={gun}></Gun>)
+                    }
+                </div>
+            </div>
+            <div className='col-lg-3 col-11 border border-info text-center p-4'>
+                <h2>Selected Guns</h2>
+                <div className='mt-5'>
+                    <button className='btn btn-light text-info border px-5 py-1 text-uppercase' variant="light">Choose 1 For Me</button>
+                    <button className='btn btn-light text-success border px-5 py-1 text-uppercase mt-3' variant="light">Choose Again</button>
+                </div>
+            </div>
         </div>
     );
 };
