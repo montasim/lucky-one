8b0c70ff68e541f9fec0d9d05651cbeea2aca14a
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-03-26 02:31:38 +0600

    cart mx-auto

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,7 +6,7 @@ const Cart = ({ item }) => {
 
     return (
         <div data-aos="fade-up"
-            data-aos-duration="3000" className='d-flex gap-5 align-items-center cart-container p-4 border border-warning mb-3'>
+            data-aos-duration="3000" className='d-flex gap-5 align-items-center cart-container p-4 border border-warning mb-3 mx-auto'>
             <img className='w-25' src={img} alt={name + ' image'} />
             <p>{name}</p>
             <h6>${price}</h6>
