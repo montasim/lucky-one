b63fa16b171d6f89bb4cbce610f9ed4b9571d95c
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2022-03-26 12:25:55 +0600

    alt text

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,7 +6,7 @@ const Cart = ({ item }) => {
     return (
         <div data-aos="fade-up"
             data-aos-duration="3000" className='d-flex gap-2 align-items-center cart-cart-container mb-3 mx-auto'>
-            <img className='cart-image-container' src={img || ''} />
+            <img className='cart-image-container' src={img || ''} alt='Find Your Favorite Gun' />
             <p>{name}</p>
             <h6>{price === '' ? '$ ' + price : ' '}</h6>
         </div>
